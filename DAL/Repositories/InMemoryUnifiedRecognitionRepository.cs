2966b18c826df5f8601cfdb55b8f6e47163546df
andy_chu <andy_chu@protech.com.tw>
2025-10-09 13:21:09 +0800

    feat: 產生不同辨識結果

--- DAL/Repositories/InMemoryUnifiedRecognitionRepository.cs
+++ DAL/Repositories/InMemoryUnifiedRecognitionRepository.cs
@@ -29,14 +29,19 @@ namespace ViscoveryDemo.DAL.Repositories
                 Data = data
             };
         }
-
+        /// <summary>
+        /// Inmemory的答案，模擬API辨識結果
+        /// 可調整回傳答案，產生三種辨識結果
+        /// </summary>
+        /// <param name="orderType"></param>
+        /// <returns></returns>
         private List<Instance> GetInstances(string orderType)
         {
             var all = new List<Instance>
             {
-                new Instance { Product = new Product { ProductName = "小麵包", ProductCode = "b101" } },
+                new Instance { Product = new Product { ProductName = "圓麵包", ProductCode = "b104" } },
                 new Instance { Product = new Product { ProductName = "三峽金牛角",   ProductCode = "b102" } },
-                new Instance { Product = new Product { ProductName = "Soft Drink",     ProductCode = "b103" } }
+                new Instance { Product = new Product { ProductName = "小圓麵包",     ProductCode = "b103" } }
             };
 
             switch (orderType)
